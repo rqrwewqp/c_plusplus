b1b268767ccfd34a13338636e7e603c0f7ca4e41
rqrwewqp <41423210@nfu.edu.tw>
2025-12-05 02:48:10 +0800

    On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   .vscode/launch.json
    	new file:   .vscode/tasks.json
    	new file:   c++.code-workspace
    	new file:   test.cpp
    	new file:   test.exe

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int x = 10;
+
+int main(){
+    int i = 10;
+    printf("Hello World\n");
+    scanf("%d", &i);
+    printf("Hello World");
+    printf("Hello World");
+    return 0;
+}
